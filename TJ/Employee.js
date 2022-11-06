9bdb16f348878291f15cc2838aba74cf6b4df5cf
Victor <victor5055@outlook.com>
2022-11-06 12:21:11 -0500

    Exporting Employee class.

--- TJ/Employee.js
+++ TJ/Employee.js
@@ -1,9 +1,26 @@
 // Code to export Employee class.
 class Employee {
-    constructor(name,id,email) {
-        this.name= name;
+    constructor(id,email,name) {
         this.id = id;
         this.email = email;
+        this.name = name;
         this.title = "Employee"
     }
+
+    getId(){
+        return this.id;
+    }
+
+    getEmail(){
+        return this.email;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getRole(){
+        return this.title;
+    }
 }
+module.exports = Employee;
