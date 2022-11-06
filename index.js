dfa9b43788085378795136899975f780b1827be0
Victor <victor5055@outlook.com>
2022-11-06 12:12:36 -0500

    Adding Employee,Engineer,Intern,Manager.

---
+++ index.js
@@ -0,0 +1,8 @@
+//Gloval variables//
+
+const Manager = require("./TJ/Manager");
+const Engineer = require("./TJ/Engineer");
+const Intern = require("./TJ/Intern");
+const path = require("path");
+const fs = require("fs");
+const inquirer = require("inquirer");
