687b65e79838797324c7bfc37935fad438eaa03c
Victor <victor5055@outlook.com>
2022-11-10 15:36:49 -0500

    Creating the Engineer html.

--- src/template.js
+++ src/template.js
@@ -1,7 +1,7 @@
 // Creating the Team//
 const generateTeam = team => {
 
-    // The Manager html//
+    // Creating The Manager html//
     const generateManager = manager => {
         return `
         <div class="card employee-card">
@@ -14,6 +14,25 @@ const generateTeam = team => {
             <li class="list-group-item">ID: ${manager.getId()}</li>
             <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
             <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+        </ul>
+    </div>
+</div>
+        `;
+    };
+
+    // Creating the Engineer html//
+    const generateEngineer = engineer => {
+        return `
+<div class="card employee-card">
+    <div class="card-header bg-primary text-white">
+        <h2 class="card-title">${engineer.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${engineer.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
         </ul>
     </div>
 </div>
