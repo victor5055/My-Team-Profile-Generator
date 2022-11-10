ff6d75901c1c8c58843dcc202264e23de502b532
Victor <victor5055@outlook.com>
2022-11-10 16:04:53 -0500

    Adding elements to Array.

--- src/template.js
+++ src/template.js
@@ -57,5 +57,24 @@ const generateTeam = team => {
 </div>
         `;
     };
+//Adding elements to Arrays//
+    const html = [];
+
+    html.push(team
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => generateManager(manager))
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => generateEngineer(engineer))
+        .join("")
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => generateIntern(intern))
+        .join("")
+    );
+
+    return html.join("");
 
 }
