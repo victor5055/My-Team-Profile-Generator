589941921f25353fa9f486b8225f203a85cd1088
Victor <victor5055@outlook.com>
2022-11-10 16:24:25 -0500

    Adding code to use inquirer to gather information from team members.

--- index.js
+++ index.js
@@ -10,3 +10,35 @@ const inquirer = require("inquirer");
 const OUTPUT_DIR = path.resolve(__dirname, "Structure");
 const outputPath = path.join(OUTPUT_DIR, "Team.html");
 const generateTeam = require("./src/template.js")
+
+// Code to use inquirer to gather information about the development team members,
+//and to create objects for each team member.//
+
+teamArray = [];
+
+function runApp () {
+
+  function createTeam () {
+    inquirer.prompt([{
+      type: "list",
+      message: "What type of employee would you like to add to your team?",
+      name: "addEmployeePrompt",
+      choices: ["Manager", "Engineer", "Intern", "No more team members are needed."]
+    }]).then(function (userInput) {
+      switch(userInput.addEmployeePrompt) {
+        case "Manager":
+          addManager();
+          break;
+        case "Engineer":
+          addEngineer();
+          break;
+        case "Intern":
+          addIntern();
+          break;
+
+        default:
+          htmlBuilder();
+      }
+    })
+  } 
+}
