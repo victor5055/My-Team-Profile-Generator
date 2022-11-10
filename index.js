9fb72e77aeb89655ded1f837a9a175f2f2588a30
Victor <victor5055@outlook.com>
2022-11-10 15:28:21 -0500

    Adding template, generating team and Manger html.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //Gloval variables//
 
-const Manager = require("./TJ/Manager");
-const Engineer = require("./TJ/Engineer");
-const Intern = require("./TJ/Intern");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const path = require("path");
 const fs = require("fs");
 const inquirer = require("inquirer");
