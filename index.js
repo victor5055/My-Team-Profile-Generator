26c3985fa49f0fc3faa9614bd6fadbd1b82dea16
Victor <victor5055@outlook.com>
2022-11-10 16:37:47 -0500

    Adding array of questions for Manager.

--- index.js
+++ index.js
@@ -41,4 +41,40 @@ function runApp () {
       }
     })
   } 
+  //Array of questions for Manager//
+  function addManager() {
+    inquirer.prompt ([
+      
+      {
+        type: "input",
+        name: "managerName",
+        message: "What is the manager's name?"
+      },
+  
+      {
+        type: "input",
+        name: "managerId",
+        message: "What is the manager's employee ID number?"
+      },
+  
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the manager's email address?"
+      },
+  
+      {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "What is the manager's office number?"
+      }
+  
+    ]).then(answers => {
+      const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+      teamArray.push(manager);
+      createTeam();
+    });
+  
+  }
+
 }
