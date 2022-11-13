453cc343267809c1cdbad7e25f7e8f374d47094d
Victor <victor5055@outlook.com>
2022-11-13 16:48:07 -0500

    Adding test for intern Chris.

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -3,19 +3,19 @@ const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
 
 test("Can create an office number.", () => {
-    const testOfficeNumber = 333;
-    const employeeInstance = new Manager("Trey", 333, "trey@company.com", testOfficeNumber);
+    const testOfficeNumber = 5055;
+    const employeeInstance = new Manager("Trey", 1, "trey@company.com", testOfficeNumber);
     expect(employeeInstance.officeNumber).toBe(testOfficeNumber);
 });
 
 test("Testing officeNumber will return office number.", () => {
-    const testOfficeNumber = 333;
-    const employeeInstance = new Manager("Trey", 333, "trey@company.com", testOfficeNumber);
+    const testOfficeNumber = 5055;
+    const employeeInstance = new Manager("Trey", 1, "trey@company.com", testOfficeNumber);
     expect(employeeInstance.getOfficeNumber()).toBe(testOfficeNumber);
 });
 
 test("Testing role.", () => {
     const returnValue = "Manager";
-    const employeeInstance = new Manager("Trey", 333, "trey@company.com", 333);
+    const employeeInstance = new Manager("Trey", 1, "trey@company.com", 1);
     expect(employeeInstance.getRole()).toBe(returnValue);
 });
